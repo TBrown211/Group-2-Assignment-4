00a8d5d424f109d77f74df3a03c7f93ea5274e27
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2023-11-19 15:58:31 -0500

    Create C# Program
    Created the obstacle mapping program

---
+++ Group 2 Assignment 4/Obstacle Mapping/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Obstacle_Mapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
